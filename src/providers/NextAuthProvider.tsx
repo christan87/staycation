23f75507bfdacc2b3b41a8d3c35f2fcdf552b215
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2025-02-12 05:59:40 -0800

    netlify error fix 018

--- src/providers/NextAuthProvider.tsx
+++ src/providers/NextAuthProvider.tsx
@@ -1,13 +1,24 @@
 'use client';
 
 import { SessionProvider } from 'next-auth/react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export function NextAuthProvider({ children }: Props) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // Prevent rendering on server
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <SessionProvider refetchInterval={0} refetchOnWindowFocus={false}>
       {children}
