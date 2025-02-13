7d744e5af76c10b8e77ce796315d0cb729b94bde
Christan Prices <christanprices@Christans-MacBook-Pro.local>
2025-02-13 07:30:43 -0800

    netlify error fix 027

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { NextAuthProvider } from "@/providers/NextAuthProvider";
 import { cn } from '@/lib/utils';
-
+//
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
